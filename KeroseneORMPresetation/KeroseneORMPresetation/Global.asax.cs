5892b2f838284e373be8b6b0045349a1160e9c7f
jerson00 <jerson.00@hotmail.com>
2016-02-18 23:21:06 -0600

    DB connection

---
+++ KeroseneORMPresetation/KeroseneORMPresetation/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using KeroseneORMPresetation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace KeroseneORMPresetation
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static IDbConnection KeroseneConnection;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            KeroseneConnection = KeroseneConection.Conection();
+        }
+    }
+}
---
+++ KeroseneORMPresetation/KeroseneORMPresetation/Models/KeroseneConection.cs
@@ -0,0 +1,24 @@
+﻿using Kerosene.ORM.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace KeroseneORMPresetation.Models
+{
+    public class KeroseneConection
+    {
+        public static IDbConnection Conection()
+        {
+            Kerosene.ORM.Direct.DataEngine.InitializeEngines();
+
+            var link = Kerosene.ORM.Direct.DataLink.Create("SqlClient",
+                "Server=JERSON-PC\\DBSERVER;Database=KeroseneDB;Integrated Security=true");
+
+            link.Open();
+            return link.DbConnection;
+
+        }
+    }
+}
